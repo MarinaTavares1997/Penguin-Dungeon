1b6cbb460e92537bdf4ce3a0ee9d5ebd434e7909
MarinaTavares1997 <marinatavares571@gmail.com>
2023-07-25 19:30:38 -0300

    Level 21

--- Penguin Dungeon/Assets/Scripts/Botao.cs
+++ Penguin Dungeon/Assets/Scripts/Botao.cs
@@ -5,12 +5,34 @@ using UnityEngine;
 public class Botao : MonoBehaviour
 {
     public bool botao;
+    public float tempo;
+    public float tempototal;
+    public GameObject blocos1;
+    public GameObject blocos2;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    // Update is called once per frame
+    void Update()
+    {
+        tempototal -= 1 * Time.deltaTime;
+
+        if(tempo <= 0)
+        {
+            blocos1.SetActive(false);
+            blocos2.SetActive(true);
+        }
+        else
+        {
+            blocos1.SetActive(true);
+            blocos2.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             botao = true;
+            tempo = tempototal;
         }
     }
 }
