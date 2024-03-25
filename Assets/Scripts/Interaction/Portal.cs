9472e66f91d879d37dc803fdfaad9af5bdf14d6b
Ayslan-gamedev <ayslanfontes@outlook.com>
2024-03-24 22:47:20 -0300

    PERF FIX - Player Movementation

---
+++ Assets/Scripts/Interaction/Portal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace PenguinDungeon
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] private Transform destination;
+        public GameObject player;
+
+        public Transform GetDestination()
+        {
+            return destination;
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.tag == "Player")
+            {
+                player.transform.position = destination.position;
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerMovementJoystick.cs
+++ Assets/Scripts/Player/PlayerMovementJoystick.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerMovementJoystick : NetworkBehaviour
+public class PlayerMovementJoystick : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D playerRb;
